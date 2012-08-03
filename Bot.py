15edcd013fb6263641301648934589887564a1e0
TomHodson <thomas.c.hodson@gmail.com>
2012-08-04 00:04:39 +0100

    major restructuring and some control over the access control

--- Bot.py
+++ Bot.py
@@ -1,57 +1,48 @@
 import Skype4Py
-import math
-import time
-from CatchSTDOut import CatchSTDOut
-sandbox_globals = {}
-sandbox_locals = {'the_answer':42}
-messagequeue = set()
 debug = False
-allowed = set((name.strip() for name in open("./allowed").read().split('\n') if name))
-if debug: print "allowed: ", allowed
+
+messagequeue = set()
 
 class BotSkypeinterface(object):
-    def __init__(self):
+    def __init__(self, commands):
         self.skype = Skype4Py.Skype(Transport='x11')
         if not self.skype.Client.IsRunning:
             print 'You need to start skype'
             exit()
+
         self.skype.FriendlyName = 'Py-in-the-Sky'
         self.skype.RegisterEventHandler('MessageStatus', self.getmessage)
         self.skype.Attach()
+        self.commands = commands
+        self.owner = 'tom.c.hodson'
+
         print "attached!" if self.skype.AttachmentStatus == 0 else "Couldn't attach to skype"
         
     def getmessage(self, message, status):
-        global messagequeue; global allowed
-        if self.iscommand(message):
-            messagequeue.add((self.iscommand(message),message,hash(self)))
-    def iscommand(self, message):
-        "return the sanitised version of the text if it's a command"
-        return message.Body[1:] if message.Body.startswith('>') else None
+        "this method gets attached to skype and called whenever a message comes in"
+        global messagequeue;
+        parsedmessage = self.commands.parse_message(message, self)
+        if parsedmessage: #parsed message returns false if it's not a command
+            messagequeue.add(parsedmessage)
+
     def __enter__(self):
         return self
     def __exit__(self, type, value, traceback):
         self.skype.UnregisterEventHandler('MessageStatus', self.getmessage)
+        self.commands.write_auth()
         del self.skype
-        
-with BotSkypeinterface() as Bot:
-    while True:
-        if messagequeue:
-            message = messagequeue.pop()
-            command = message[0]
-            if  message[1].Sender.Handle in allowed:
-                out = []
-                err = []
-                sandbox_locals['message'] = message[1]
-                with CatchSTDOut(out, err):
-                    exec command in sandbox_globals, sandbox_locals
-                response = str(err[0]) if err[0] else " ".join(out)
-                if debug: response = response + "\nInstanceId:" + str(message[2])
-                message[1].Chat.SendMessage(response)
-            else:
-                message[1].Chat.SendMessage("{} ({}) not authorised".format(message[1].Sender.FullName,
-                                                                            message[1].Sender.Handle
-                                                                            ))
-        else:
-            #print "I'm alive"
-            time.sleep(0.5)
-        
+
+if __name__ == '__main__':
+    from Commands import DefaultCommands as Commands
+    import time
+
+    with  open('./allowed', 'r+') as auth:
+        with BotSkypeinterface(Commands(auth)) as Bot:
+            while True:
+                if messagequeue:
+                    function, args = messagequeue.pop()
+                    function(*args)
+                else:
+                    if debug: print "I'm still alive"
+                    time.sleep(0.5)
+                
---
+++ Commands.py
@@ -0,0 +1,66 @@
+from CatchSTDOut import CatchSTDOut
+
+class BaseCommands(object):
+	def __init__(self, authfile):
+		self.authorised = set((name.strip() for name in authfile.read().split('\n') if name))
+		self.authfile = authfile
+		self.sandbox_globals = {}
+		self.sandbox_locals = {'the_answer':42}
+	
+	def parse_message(self, message, bot):
+		"return (function, args) if it's a command where function(args) carries out the command"
+		for key in self.commandlist:
+			if message.Body.startswith(key):
+				if message.Sender.Handle not in self.authorised: 
+					self.deny(message)
+					return False
+				else:
+					return (self.commandlist[key],  (message,bot))
+	def deny(self, message):
+		message.Chat.SendMessage("{} ({}) not authorised".format(message.Sender.FullName, message.Sender.Handle))
+	def write_auth(self):
+		print 'saving to authfile'
+		self.authfile.seek(0)
+		self.authfile.write("\n".join(self.authorised))
+		self.authfile.truncate()
+
+class DefaultCommands(BaseCommands):
+	def __init__(self, authfile):
+			self.commandlist = {'>':self.run_as_python, '<':self.utils}
+			super(DefaultCommands, self).__init__(authfile)
+
+	def run_as_python(self, message, bot):
+		out = []
+		err = []
+		command = message.Body[1:]
+		with CatchSTDOut(out, err):
+			exec command in self.sandbox_globals, self.sandbox_locals
+		response = str(err[0]) if err[0] else " ".join(out)
+		message.Chat.SendMessage(response)
+	def utils(self, message, bot):
+		commandparts = message.Body[1:].split()
+		if message.Sender.Handle != bot.owner:
+			deny(message)
+			return
+
+		if commandparts[0] == 'auth':
+			if commandparts[1] == 'add':
+				users = commandparts[2:]
+				self.authorised.update(set(users))
+				message.Chat.SendMessage("{} authorised".format(', '.join(users)))
+			elif commandparts[1] == 'remove':
+				users = commandparts[2:]
+				self.authorised.difference_update(set(users))
+				message.Chat.SendMessage("{} removed".format(', '.join(users)))
+
+			elif commandparts[1] == 'list':
+				message.Chat.SendMessage("These people are authorised:\n {}".format(', '.join(self.authorised)))
+
+		elif commandparts[0] == 'exit':
+			message.Chat.SendMessage("exiting")
+			exit()
+
+
+if __name__ == '__main__':
+	pass
+ #TODO: write dummy message and bot objects to test this module with
